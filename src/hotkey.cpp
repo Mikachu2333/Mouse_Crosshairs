f6cce3e2b701b1d7972fc4671ecf150245078e53
Mikachu2333 <mikachu.23333@zohomail.com>
2025-08-06 13:41:30 +0800

    add version

--- src/hotkey.cpp
+++ src/hotkey.cpp
@@ -1,23 +1,9 @@
 #include "./hotkey.h"
 
-// 热键 ID 常量
-#define HOTKEY_ID 2039166592   // 显示/隐藏热键 ID
-#define HOTKEY_ID2 2078194518  // 退出程序热键 ID
-
 // 注册显示/隐藏和退出热键
 void HotkeyManager::RegisterToggleHotkey(const HotkeyConfig &cfg_h_s,const HotkeyConfig &cfg_exit) {
     RegisterHotKey(nullptr, HOTKEY_ID, cfg_h_s.mod, cfg_h_s.vk);
     RegisterHotKey(nullptr, HOTKEY_ID2, cfg_exit.mod, cfg_exit.vk);
-}
-
-// 检查消息是否为显示/隐藏热键
-bool HotkeyManager::IsToggleHotkey(const MSG &msg) {
-    return msg.message == WM_HOTKEY && msg.wParam == HOTKEY_ID;
-}
-
-// 检查消息是否为退出热键
-bool HotkeyManager::IsExitHotkey(const MSG &msg) {
-    return msg.message == WM_HOTKEY && msg.wParam == HOTKEY_ID2;
 }
 
 // 注销所有热键
--- src/hotkey.h
+++ src/hotkey.h
@@ -2,13 +2,13 @@
 #include <windows.h>
 #include "config.h"
 
+// 热键 ID 常量
+#define HOTKEY_ID 2039166592   // 显示/隐藏热键 ID
+#define HOTKEY_ID2 2078194518  // 退出程序热键 ID
+
 class HotkeyManager {
 public:
     static void RegisterToggleHotkey(const HotkeyConfig &cfg_h_s,const HotkeyConfig &cfg_exit);
-
-    static bool IsToggleHotkey(const MSG &msg);
-
-    static bool IsExitHotkey(const MSG &msg);
 
     static void UnregisterAll();
 };
